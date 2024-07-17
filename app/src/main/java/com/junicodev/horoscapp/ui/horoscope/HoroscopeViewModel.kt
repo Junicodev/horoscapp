ebb0c4d1f004b424fcabf0d1a72e19e939465324
Juan López <junicodevv@gmail.com>
2024-07-17 07:41:48 +0200

    Adapter horoscope

--- app/src/main/java/com/junicodev/horoscapp/ui/horoscope/HoroscopeViewModel.kt
+++ app/src/main/java/com/junicodev/horoscapp/ui/horoscope/HoroscopeViewModel.kt
@@ -1,9 +1,22 @@
 package com.junicodev.horoscapp.ui.horoscope
 
 import androidx.lifecycle.ViewModel
+import com.junicodev.horoscapp.domain.model.HoroscopeInfo
+import com.junicodev.horoscapp.domain.model.HoroscopeInfo.Aries
+import com.junicodev.horoscapp.domain.model.HoroscopeInfo.Cancer
+import com.junicodev.horoscapp.domain.model.HoroscopeInfo.Taurus
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class HoroscopeViewModel @Inject constructor() : ViewModel() {
+
+    private var _horoscope = MutableStateFlow<List<HoroscopeInfo>>(emptyList())
+    val horoscope: StateFlow<List<HoroscopeInfo>> = _horoscope
+
+    init {
+        _horoscope.value = listOf(Aries, Taurus, Cancer)
+    }
 }
